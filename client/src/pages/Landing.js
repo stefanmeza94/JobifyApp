59664e7dbd47a5b680b771d64ca88a6fdcc0ff72
Stefan Meza <stefanmeza93@gmail.com>
2022-05-05 17:04:16 +0200

    Add routes for pages

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/Testing';
-import Logo from '../components/Logo';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
@@ -19,7 +20,9 @@ const Landing = () => {
             forage mlkshk. Meggings kitsch bitters umami cray, listicle
             semiotics disrupt tofu fam fashion axe.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
