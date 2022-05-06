6529fdfb5c253bbabd7341da8459fc3f26d32e8b
Stefan Meza <stefanmeza93@gmail.com>
2022-05-06 09:59:33 +0200

    Add custom componenet for formrow

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = e => {
+    console.log(e.target);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
