b18504a0648236440b3215e9bffec848c8cc0369
Stefan Meza <stefanmeza93@gmail.com>
2022-05-06 10:01:54 +0200

    Add register component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -26,7 +26,27 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
-
+        {/* name input */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type="submit" className="btn btn-block">
           Submit
         </button>
