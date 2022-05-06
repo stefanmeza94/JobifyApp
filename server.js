bf6d7f08906843f2f5642d0c8b81a72b615a5bd8
Stefan Meza <stefanmeza93@gmail.com>
2022-05-06 16:35:49 +0200

    Add conrollers and routes for jobs endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 
-// dotnenv je zapravo paket koji ce da nam omogcu da ucitamo sve iz .env fajla (kad god menajmo nesto u .env fajlu moramo da restartujemo server)
+// dotnenv je zapravo paket koji ce da nam omogcu da ucitamo sve iz .env fajla kroz process.env (kad god menajmo nesto u .env fajlu moramo da restartujemo server)
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,11 +10,13 @@ import connectDB from './db/connect.js';
 
 // routers
 import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobRoutes.js';
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+// middleware koji ce da nam pomogne da da ocitamo json koji nam stize post metodom sa klijentske strane
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -22,6 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 //  app.use ce da se pokrene za sve rute koje budemo imali .get, .post, .put, .patch... ali smestamo ga ispod nasih ruta da prvo proveri njih i ukoliko se odkucana ruta ne poklapa ni sa jednom nasom onda ce da pokrene ovaj middleware notFoundMiddlware;
 app.use(notFoundMiddleware);
@@ -29,6 +32,7 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
+// prvo zelimo da se konektujemo sa mongo db bazom pa tek onda da pokrenemo nas server
 const start = async () => {
   try {
     await connectDB(process.env.MONOGO_URL);
