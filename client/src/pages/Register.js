08c1d42ebd03234a48715e6e438646dc9cd29149
Stefan Meza <stefanmeza93@gmail.com>
2022-05-06 10:33:22 +0200

    Add toggle between login and register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react';
-import { Logo, FormRow } from '../components';
+import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
   name: '',
   email: '',
   password: '',
-  isMember: true,
+  isMember: false,
+  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
 
   const handleChange = e => {
     console.log(e.target);
@@ -25,14 +30,17 @@ const Register = () => {
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+        {values.showAlert && <Alert />}
         {/* name input */}
-        <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         {/* email input */}
         <FormRow
           type="email"
@@ -50,6 +58,13 @@ const Register = () => {
         <button type="submit" className="btn btn-block">
           Submit
         </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
