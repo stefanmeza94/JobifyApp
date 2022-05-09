e090f9dcd8b8e189c9f881f6104ad2de9c47ca93
Stefan Meza <stefanmeza93@gmail.com>
2022-05-09 11:27:30 +0200

    Add base error handlers

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 
 const register = async (req, res) => {
-  // posto koristmo paket express-async-errors ne moramo da koristimo ovde trycatch block samim tim ne moramo da koristmo nexto() funkciju koja ce da gadja sledec middleware, taj paket ce za nas u pozadini sam da prosledjuje gresku nasem middlewaru (error-handler.js unutar middleware foldera)
+  // posto koristmo paket express-async-errors ne moramo da koristimo ovde trycatch block samim tim ne moramo da koristmo next() funkciju koja ce da gadja sledeci middleware, taj paket ce za nas u pozadini sam da prosledjuje gresku nasem middlewaru (error-handler.js unutar middleware foldera)
   const user = await User.create(req.body);
   // ovde koristimo paket http-status-codes koji ce unutar ovih konstati (CREATE, INTERNAL_SERVER_ERROR  itd) da nam vraca adekvatne brojeve gresaka.
   res.status(StatusCodes.CREATED).json({ user });
