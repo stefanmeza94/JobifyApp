094816434fdc8b5f8f24fa14cf3d84b8f2a6f791
Stefan Meza <stefanmeza93@gmail.com>
2022-05-10 11:04:17 +0200

    Add concurrently package to run both server and client at the same time

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,18 @@
-import User from "../models/User.js";
-import { StatusCodes } from "http-status-codes";
-import { BadRequestError } from "../errors/index.js";
+import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
 
 // ako nasa baza bude kompromitovana nas password ce da bude na izvolte napdacu ako stoji kao string, zato je bolja opcija da pre nego sto posaljemo nasu sifru u bazu (monogoDB) da je hash-ujemo. Jednom kada hash-ujemo password ne mozemo vise da ga odhashujemo, jedino sto mozemo jeste da ga storujemo tako hashovan u bazu podataka i prilikom logovanja da uporedmimo uneti password sa nasim hasovanim
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new BadRequestError("please provide all values");
+    throw new BadRequestError('please provide all values');
   }
 
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
-    throw new BadRequestError("Email already in use");
+    throw new BadRequestError('Email already in use');
   }
 
   // posto koristmo paket express-async-errors ne moramo da koristimo ovde trycatch block samim tim ne moramo da koristmo next() funkciju koja ce da gadja sledeci middleware, taj paket ce za nas u pozadini sam da prosledjuje gresku nasem middlewaru (error-handler.js unutar middleware foldera)
@@ -20,25 +20,24 @@ const register = async (req, res) => {
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
   // ovde koristimo paket http-status-codes koji ce unutar ovih konstati (CREATE, INTERNAL_SERVER_ERROR  itd) da nam vraca adekvatne brojeve gresaka.
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      user: {
-        email: user.email,
-        lastName: user.lastName,
-        location: user.location,
-        name: user.name,
-      },
-      token,
-    });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  res.send('login user');
 };
 
 const updateUser = async (req, res) => {
-  res.send("update User");
+  res.send('update User');
 };
 
 export { register, login, updateUser };
--- models/User.js
+++ models/User.js
@@ -1,48 +1,49 @@
-import mongoose from "mongoose";
-import validator from "validator";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide name"],
+    required: [true, 'Please provide name'],
     minlength: 3,
     maxlength: 20,
     trim: true,
   },
   email: {
     type: String,
-    required: [true, "Please provide email"],
+    required: [true, 'Please provide email'],
     validate: {
       validator: validator.isEmail,
-      message: "Please provide valid email",
+      message: 'Please provide valid email',
     },
     unique: true,
   },
   password: {
     type: String,
-    required: [true, "Please provide password"],
+    required: [true, 'Please provide password'],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
     trim: true,
     maxlength: 20,
-    default: "lastName",
+    default: 'lastName',
   },
   location: {
     type: String,
     trim: true,
     maxlength: 20,
-    default: "my city",
+    default: 'my city',
   },
 });
 
-// pre save hook koji ce da se pokrene pre nego sto kreiramo model usera
+// pre save hook koji ce da se pokrene pre nego sto kreiramo model usera i pre nego sto ovaj password ode u bazu podataka na monogoDB
 // mora da bude funkcijska deklaracija zato sto cemo da koristimo THIS (arrow funkcije prilikom izvrsavanja ne dobijaju this varijablu)
 // pre svakog kreiranja skime usera u bazu pokretace se ova pre() funkcija koja ce za nas da hashuje password.
-UserSchema.pre("save", async function () {
+UserSchema.pre('save', async function () {
   // ovaj salt ce da generise neke radnom brojeve i slova
   // sto veci broj prosledimo genSalt() funkciji to biti bolje zasticen password medjutim s druge strane trebace vise vremena da se to odradi!
   const salt = await bcrypt.genSalt(10);
@@ -50,10 +51,11 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// kreirmao metodu instance userSchema (ovo createJWT mozemo da nazovemo kako god zelimo)
 UserSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
   });
 };
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.model('User', UserSchema);
