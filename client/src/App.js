afabe9782c80bd35ee9748774309e66d26280f62
Stefan Meza <stefanmeza93@gmail.com>
2022-05-12 18:29:01 +0200

    Add nested routes for main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Register, Landing, Error } from './pages';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register, Landing, Error } from "./pages";
 import {
   AllJobs,
   Profile,
   SharedLayout,
   Stats,
   AddJob,
-} from './pages/dashboard';
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>dashboard</div>} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,5 +1,16 @@
+import { Outlet, Link } from "react-router-dom";
+import Wrapper from "../../assets/wrappers/SharedLayout";
+
 const SharedLayout = () => {
-  return <div>shared layout</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Link to="add-job">add job</Link>
+        <Link to="all-jobs">all jobs</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
