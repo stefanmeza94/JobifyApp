22541c44d0056db081d2baba84cadb57ce353d0c
Stefan Meza <stefanmeza93@gmail.com>
2022-05-13 16:17:34 +0200

    Add sidebar for small devices

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,47 @@
+import { NavLink } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import Logo from './Logo';
+import { FaTimes } from 'react-icons/fa';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h1>smallsidebar</h1>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button className="close-btn" type="button" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, id, path, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={toggleSidebar}
+                  className={({ isActive }) => {
+                    return isActive ? 'nav-link active' : 'nav-link';
+                  }}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
