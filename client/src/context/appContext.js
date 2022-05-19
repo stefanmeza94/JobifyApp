b9a1f215ddd6389da3f8d17b5cb597fd119faac4
Stefan Meza <stefanmeza93@gmail.com>
2022-05-19 10:32:26 +0200

    Add edit job functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -20,6 +20,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -200,7 +201,11 @@ const AppProvider = ({ children }) => {
   };
 
   const setEditJob = (id) => {
-    console.log(`set edit job: ${id}`);
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+
+  const editJob = () => {
+    console.log('edit job');
   };
 
   const deleteJob = (id) => {
@@ -222,6 +227,7 @@ const AppProvider = ({ children }) => {
         getJobs,
         setEditJob,
         deleteJob,
+        editJob,
       }}
     >
       {children}
