a736a8bf6bf3b154c7504f67811f2031ef6d3c9e
Stefan Meza <stefanmeza93@gmail.com>
2022-05-19 11:10:25 +0200

    Add updateJob controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,6 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
+import { BadRequestError, UnAuthenticatedError, NotFoundError } from '../errors/index.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -25,7 +25,27 @@ const deleteJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send('update job');
+  const { id: jobId } = req.params;
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`There is no job with id: ${jobId}`);
+  }
+
+  // check permissions
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const showStats = async (req, res) => {
