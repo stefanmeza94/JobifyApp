750e0cf7fa504f8c3f8ad77f5796598886b9a9f5
Stefan Meza <stefanmeza93@gmail.com>
2022-05-20 11:15:28 +0200

    Add aggregation pipelines for show stats controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,6 +2,7 @@ import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, UnAuthenticatedError, NotFoundError } from '../errors/index.js';
 import checkPermissions from '../utils/checkPermissions.js';
+import mongoose from 'mongoose';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -70,7 +71,26 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res.send('show stats');
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let montlyApplications = [];
+
+  res.status(StatusCodes.OK).json({ defaultStats, montlyApplications });
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
